4fd4b3d32f03f14aa314e7bb8a5d97c75d541846
ny ony <ninihrkt@gmail.com>
2023-01-26 11:53:18 +0100

    Initial commit

---
+++ src/FenetreAchat.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FenetreAchat extends JFrame implements ActionListener {
+
+	private JButton btAchat;
+	private JTextField txtQuantite;
+	private JComboBox<String> combo;
+
+	public FenetreAchat(String[] lesProduits) {
+
+		setTitle("Achat");
+		setBounds(500, 500, 200, 125);
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new FlowLayout());
+		btAchat = new JButton("Achat");
+		txtQuantite = new JTextField(5);
+		txtQuantite.setText("0");
+
+		combo = new JComboBox<String>(lesProduits);
+		combo.setPreferredSize(new Dimension(100, 20));
+		contentPane.add(new JLabel("Produit"));
+		contentPane.add(combo);
+		contentPane.add(new JLabel("Quantité achetée"));
+		contentPane.add(txtQuantite);
+		contentPane.add(btAchat);
+
+		btAchat.addActionListener(this);
+
+		this.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		this.dispose();
+	}
+
+}
